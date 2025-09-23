891a1fe2d3da198076de8d78a00b15e0db331f89
DESKTOP-PG84347\USER <heomin000518@gamil.com>
2025-09-23 09:44:17 +0900

    lets go

--- online_hackathon_backend/main.py
+++ online_hackathon_backend/main.py
@@ -1,8 +1,12 @@
 from fastapi import FastAPI
 app = FastAPI()
 
+from fastapi.responses import FileResponse
 
-@app.get("/")
+
+# uvicorn main:app --reload
+# 이거 복사해서 사용
+@app.get("/") 
 def hello():
     return 'hello'
 
